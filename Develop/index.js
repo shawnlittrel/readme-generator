e0bc43400d71fe533737ebfacb1d484ddb9e0434
Shawn Littrel <shawnlittrel@protonmail.com>
2021-03-25 12:19:17 -0700

    initial layout and pseudocode

---
+++ Develop/index.js
@@ -0,0 +1,32 @@
+// TODO: Include packages needed for this application
+//Which packages are needed?  Inquirer, fs, other modules written
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+//which sections are needed in a readme?
+//Project title - Title
+//Description of project - Description
+//Maybe table of contents - Table of Contents
+//Maybe installation steps - Installation
+//How to use app - Usage
+//Maybe credits - Credits
+//License information - Licensing
+//^^List of options
+//Maybe badges - Badges
+//^^Add to top of page
+//Maybe features - Features
+//Maybe testing information - Testing
+//Where to ask questions - Questions/Comments
+//^^Need Github username and an email address
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
